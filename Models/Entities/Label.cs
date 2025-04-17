e4eb55d9cce3090cd69ee26d4d2fcf3678f693d0
Dũng Vũ <88957690+DungVipno@users.noreply.github.com>
2025-04-17 12:56:38 +0700

    first commit

---
+++ Models/Entities/Label.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    public class Label
+    {
+        public int LabelId { get; set; }
+        public int LabelName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ Models/Entities/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public int Title { get; set; }
+
+        public int? Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int LabelId { get; set; }
+        public Label? Label { get; set; }
+    }
+}
